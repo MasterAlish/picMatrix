3552f4117f224c2a708f6fff0deae18f5983d7f3
alisher <masteraalish@gmail.com>
2015-07-02 14:04:41 +0600

    #1 #2 Add simplest algorythm to round weights

--- project_matrix/main.py
+++ project_matrix/main.py
@@ -1,5 +1,14 @@
+import random
 from pic_matrix import PicMatrix
 
-test_pic_weights = [155, 10, 19, 5, 5, 36, 90, 50, 140, 43]
 
-PicMatrix(test_pic_weights).run()
+def add_to_arr_random_number(a):
+    if random.randint(1,4) <3:
+        a.append(random.randint(1, 500))
+    else:
+        a.append(random.randint(1, 10))
+    return a
+
+for i in range(20):
+    test_pic_weights = reduce(lambda arr, e: add_to_arr_random_number(arr), range(50), [])
+    PicMatrix(test_pic_weights).run()
